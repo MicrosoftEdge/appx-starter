d3a4da3f1fe0fa39fef15bbc5e189cdc58d1a7b9
Alex Lu <alxlu@users.noreply.github.com>
2015-05-13 15:21:39 -0700

    SID Test

--- gulpfile.js/AppxUtilities/Start.ps1
+++ gulpfile.js/AppxUtilities/Start.ps1
@@ -1,11 +1,14 @@
 $cwd = $args[0]
 cd $cwd
-$AddAppxPackageExt = $cwd + "\gulpfile.js\AppxUtilities\Add-AppxPackageExt.ps1"
-<#
-$manifest = $cwd + "\src\AppxManifest.xml"
-Set-ExecutionPolicy Unrestricted
-Write-Output $manifest
-Invoke-Expression $AddAppxPackageExt $manifest
-#>
-
-.\gulpfile.js\AppxUtilities\Add-AppxPackageExt.ps1 .\src\AppxManifest.xml
+$result = .\gulpfile.js\AppxUtilities\Add-AppxPackageExt.ps1 .\src\AppxManifest.xml
+$pfn = $result.Package.PackageFamilyName
+Push-Location
+Set-Location 'HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Mappings'
+$apps = ls
+$list = $apps | ForEach-Object {Get-ItemProperty $_.pspath}
+#Moniker: contosoapptest_h91ms92gdsmmt
+$list | ForEach-Object {if ($_.Moniker -eq $pfn) { $sid=$_.PSChildName}}
+Write-Host $sid
+Pop-Location
+CheckNetIsolation loopbackexempt -a -p=$sid
+$result | .\gulpfile.js\AppxUtilities\Launch-AppxPackage.ps1
