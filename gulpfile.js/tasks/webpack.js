fd86810b1a0533941d9ffeb5b760ef2f07c333f6
Alex Lu <alxlu@users.noreply.github.com>
2015-08-12 14:51:14 -0700

    fix webpack watch task

--- gulpfile.js/tasks/webpack.js
+++ gulpfile.js/tasks/webpack.js
@@ -5,17 +5,12 @@ var webpack = require('webpack');
 var gutil = require('gulp-util');
 
 gulp.task('webpack', function(done) {
-  var built = false;
   if (config.watch) {
     webpack(config.webpack).watch(200, function(err, stats) {
       if (err) {
         console.log(err);
       }
-      if (!built) {
-        built = true;
-        gutil.log(gutil.colors.cyan('webpack'), 'task watching files...');
-        done();
-      }
+      gutil.log(gutil.colors.cyan('webpack'), 'task watching files...');
     });
   } else {
     webpack(config.webpack, function(err, stats) {
