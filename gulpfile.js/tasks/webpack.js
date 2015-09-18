abc7311b055522571288ccade76e37bee2da80aa
Alex Lu <alxlu@users.noreply.github.com>
2015-09-18 12:42:32 -0700

    properly log webpack build errors

--- gulpfile.js/tasks/webpack.js
+++ gulpfile.js/tasks/webpack.js
@@ -4,19 +4,25 @@ var gulp = require('gulp');
 var webpack = require('webpack');
 var gutil = require('gulp-util');
 
+function checkErrors(err, stats) {
+  if (err) {
+    throw new gutil.PluginError('webpack', err);
+    gutil.log('[webpack]', stats.toString());
+  }
+  if (stats.compilation.errors.length) {
+    gutil.log(stats.compilation.errors[0].toString());
+  }
+}
+
 gulp.task('webpack', function(done) {
   if (config.watch) {
     webpack(config.webpack).watch(200, function(err, stats) {
-      if (err) {
-        console.log(err);
-      }
+      checkErrors(err, stats);
       gutil.log(gutil.colors.cyan('webpack'), 'task watching files...');
     });
   } else {
     webpack(config.webpack, function(err, stats) {
-      if (err) {
-        console.log(err);
-      }
+      checkErrors(err, stats);
       done();
     });
   }
