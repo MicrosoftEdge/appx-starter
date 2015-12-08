b6273e028deb79b720c4cfeebfc720fe65643538
Alex Lu <alxlu@users.noreply.github.com>
2015-12-08 11:12:57 -0800

    fix appx task

--- gulpfile.babel.js/tasks/appx.js
+++ gulpfile.babel.js/tasks/appx.js
@@ -3,18 +3,18 @@ import config from '../config';
 import * as path from 'path';
 import * as os from 'os';
 
-let registerApp = null;
+let hwa = null;
 if (os.platform() === 'win32') {
   try {
-    registerApp = require('hwa');
+    hwa = require('hwa');
   } catch (err) {
     console.log(err);
   }
 }
 
 gulp.task('appx', done => {
-  if (registerApp) {
-    registerApp(path.resolve(config.appx.src));
+  if (hwa) {
+    hwa.registerApp(path.resolve(config.appx.src));
   } else {
     console.log('You need to be running Windows 10 launch the app');
   }
