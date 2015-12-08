442cfeb0e8d2def7b46341fbfbd5cd34881aecb4
Alex Lu <alxlu@users.noreply.github.com>
2015-12-08 10:31:45 -0800

    make hwa optionaldep; modify appx task with fallback

--- gulpfile.babel.js/tasks/appx.js
+++ gulpfile.babel.js/tasks/appx.js
@@ -1,9 +1,23 @@
 import gulp from 'gulp';
 import config from '../config';
 import * as path from 'path';
-import {registerApp} from 'hwa';
+import * as os from 'os';
+
+let registerApp = null;
+if (os.platform() === 'win32') {
+  try {
+    registerApp = require('hwa');
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 gulp.task('appx', done => {
-  registerApp(path.resolve(config.appx.src));
+  if (registerApp) {
+    registerApp(path.resolve(config.appx.src));
+  } else {
+    console.log('You need to be running Windows 10 launch the app');
+  }
+
   done();
 });
